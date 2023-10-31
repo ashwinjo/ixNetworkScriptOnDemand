ea274d3f4d413e6d2f563b22b86eed5869a8a5f3
Ashwin Joshi <ashwin.joshi@keysight.com>
2023-10-31 12:02:20 -0700

    First commit

---
+++ commander.py
@@ -0,0 +1,65 @@
+import click
+from sod import SessionManager
+
+@click.command()
+@click.option('--session_name', help="Give your IxNetwork Session Unique Name")
+@click.option('--linux_api_server', default='', help="Give your IxNetwork Session Unique Name")
+@click.option('--linux_api_server_username', default='admin', help="Give your IxNetwork Session Unique Name")
+@click.option('--linux_api_server_password', default='admin', help="Give your IxNetwork Session Unique Name")
+@click.option('--source_chassis_card_port', default='', help='10.10.10.10;1;5')
+@click.option('--dest_chassis_card_port', default='', help='10.10.10.10;1;6')
+@click.option('--linerate', default='', help='50')
+@click.option('--packetsize', default='', help='9000')
+@click.option('--ixiaconfigfilepath', default='', help='9000')
+@click.option('--operation', help='setupSession | startTraffic | stopTraffic | clearCounters| setLineRate | setPacketSize| showPacketLoss')
+def parse_args(session_name, linux_api_server, linux_api_server_username, linux_api_server_password,
+               source_chassis_card_port, dest_chassis_card_port, linerate, packetsize, ixiaconfigfilepath, operation):
+
+    if operation == "setupSession":
+        sm = SessionManager(linux_api_server=linux_api_server, session_name=session_name, 
+                            username='admin', password=linux_api_server_password)
+        sm.create_session_object()
+        sm.load_config_in_session(ixiaconfigfilepath)
+        sm.map_and_connect_ports(source_chassis_card_port, dest_chassis_card_port)
+        sm.verify_control_plane_up()
+    
+    if operation == "startTraffic":
+        sm = SessionManager(linux_api_server=linux_api_server, session_name=session_name, 
+                            username='admin', password=linux_api_server_password)
+        sm.start_traffic()
+    
+    if operation == "stopTraffic":
+        sm = SessionManager(linux_api_server=linux_api_server, session_name=session_name, 
+                            username='admin', password=linux_api_server_password)
+        sm.stop_traffic()
+
+    if operation == "setLineRate":
+        sm = SessionManager(linux_api_server=linux_api_server, session_name=session_name, 
+                            username='admin', password=linux_api_server_password)
+        sm.set_linerate(linerate)
+    
+    if operation == "setPacketSize":
+        sm = SessionManager(linux_api_server=linux_api_server, session_name=session_name, 
+                            username='admin', password=linux_api_server_password)
+        sm.set_packetsize(packetsize)
+    
+    if operation == "clearCounters":
+        sm = SessionManager(linux_api_server=linux_api_server, session_name=session_name, 
+                            username='admin', password=linux_api_server_password)
+        sm.clear_counters()
+
+    if operation == "showPacketLoss":
+        sm = SessionManager(linux_api_server=linux_api_server, session_name=session_name,
+                             username='admin', password=linux_api_server_password)
+        sm.show_statistics()
+
+    if operation == "removeSession":
+        sm = SessionManager(linux_api_server=linux_api_server, session_name=session_name,
+                             username='admin', password=linux_api_server_password)
+        sm.delete_session_object()
+
+if __name__ == '__main__':
+    parse_args()
+
+
+    
---
+++ sod.py
@@ -0,0 +1,142 @@
+from ixnetwork_restpy import SessionAssistant, Files, BatchUpdate
+
+class TrafficGenerator():
+    def __init__(self, ixNetwork=None):
+        self.ixNetwork = ixNetwork
+
+    def start_traffic(self):
+        traffic_items = self.ixNetwork.Traffic.TrafficItem.find()
+        self.ixNetwork.Traffic.Apply()
+        self.ixNetwork.Traffic.StartStatelessTrafficBlocking()
+
+    def stop_traffic(self):
+        self.ixNetwork.Traffic.StopStatelessTrafficBlocking()
+
+    def clear_counters(self):
+        self.ixNetwork.ClearPortsAndTrafficStats()
+    
+    def set_linerate(self, linerate=None):
+        traffic_items = self.ixNetwork.Traffic.TrafficItem.find()
+        # we are using batch update to update some of traffic parameters and parameters of its sub child
+        with BatchUpdate(self.ixNetwork):
+            for idx, traffic_item in enumerate(traffic_items):
+                flgroups = traffic_item.HighLevelStream.find()
+                flgroups.FrameRate.find().Rate = linerate
+        self.ixNetwork.Traffic.TrafficItem.find().Generate()
+                
+    def set_packetsize(self, packetsize=None):
+        traffic_items = self.ixNetwork.Traffic.TrafficItem.find()
+        # we are using batch update to update some of traffic parameters and parameters of its sub child
+        with BatchUpdate(self.ixNetwork):
+            for idx, traffic_item in enumerate(traffic_items):
+                flgroups = traffic_item.HighLevelStream.find()
+                flgroups.FrameSize.find().FixedSize = str(packetsize)
+                flgroups.FrameSize.find().Type = "fixed"
+        self.ixNetwork.Traffic.TrafficItem.find().Generate()
+
+        
+class SessionManager():
+    def __init__(self, linux_api_server=None, session_name=None, 
+                 username=None, password=None) -> None:
+        self.linux_api_server = linux_api_server
+        self.session_name = session_name
+        self.username = username
+        self.password = password
+    
+    def create_session_object(self):
+        SessionAssistant(IpAddress=self.linux_api_server, RestPort=None, UserName=self.username, 
+                                   Password=self.password, 
+                                   SessionName=self.session_name, SessionId=None, 
+                                   ApiKey=None,
+                                   ClearConfig=False, 
+                                   LogLevel='info', 
+                                   LogFilename='restpy.log')
+        
+    def delete_session_object(self):
+        session = self.get_session_object()
+        session.Session.remove()
+
+
+    def get_session_object(self):
+        return SessionAssistant(IpAddress=self.linux_api_server, RestPort=None, UserName=self.username, 
+                                Password=self.password, 
+                                SessionName=self.session_name, SessionId=None, 
+                                ApiKey=None,
+                                ClearConfig=False, 
+                                LogLevel='info', 
+                                LogFilename='restpy.log')
+
+    def load_config_in_session(self, ixiaconfigfilepath):
+        session = self.get_session_object() 
+        ixNetwork = session.Ixnetwork
+        ixNetwork.info('Loading config file: {0}'.format(ixiaconfigfilepath))
+        ixNetwork.LoadConfig(Files(ixiaconfigfilepath, local_file=True))
+
+
+    def map_and_connect_ports(self, source_chassis_card_port, dest_chassis_card_port):
+        session = self.get_session_object() 
+        ixNetwork = session.Ixnetwork
+        # Assign ports. Map physical ports to the configured vports.
+        chassis_a, card_a, port_a = source_chassis_card_port.split(";")
+        chassis_b, card_b, port_b = dest_chassis_card_port.split(";")
+        portList = [[chassis_a, card_a, port_a], [chassis_b, card_b, port_b]]
+
+        portMap = session.PortMapAssistant()
+        for index,port in enumerate(portList):
+            # For the port name, get the loaded configuration's port name
+            portName = ixNetwork.Vport.find()[index].Name
+            portMap.Map(IpAddress=port[0], CardId=port[1], PortId=port[2], Name=portName)
+            
+        portMap.Connect(True)
+    
+    def verify_control_plane_up(self):
+        session = self.get_session_object() 
+        ixNetwork = session.Ixnetwork
+        ixNetwork.StartAllProtocols(Arg1='sync')
+        ixNetwork.info('Verify protocol sessions\n')
+        protocolSummary = session.StatViewAssistant('Protocols Summary')
+        protocolSummary.CheckCondition('Sessions Not Started', protocolSummary.EQUAL, 0)
+        protocolSummary.CheckCondition('Sessions Down', protocolSummary.EQUAL, 0)
+        ixNetwork.info(protocolSummary)
+
+    def show_statistics(self):
+        session = self.get_session_object() 
+        flowStatistics = session.StatViewAssistant('Flow Statistics')
+        print ('Tx Port', 'Rx Port', 'Loss %')
+        for rowNumber,flowStat in enumerate(flowStatistics.Rows):
+            print (flowStat['Tx Port'], flowStat['Rx Port'], flowStat['Loss %'])
+                   
+    def start_traffic(self):
+        session = self.get_session_object() 
+        ixNetwork = session.Ixnetwork
+        tg = TrafficGenerator(ixNetwork=ixNetwork)
+        tg.start_traffic()
+
+    def stop_traffic(self):
+        session = self.get_session_object() 
+        ixNetwork = session.Ixnetwork
+        tg = TrafficGenerator(ixNetwork=ixNetwork)
+        tg.stop_traffic()
+    
+    def set_linerate(self, linerate):
+        session = self.get_session_object() 
+        ixNetwork = session.Ixnetwork
+        tg = TrafficGenerator(ixNetwork=ixNetwork)
+        tg.set_linerate(linerate)
+    
+    def set_packetsize(self, packetsize):
+        session = self.get_session_object() 
+        ixNetwork = session.Ixnetwork
+        tg = TrafficGenerator(ixNetwork=ixNetwork)
+        tg.set_packetsize(packetsize)
+
+    def clear_counters(self):
+        session = self.get_session_object() 
+        ixNetwork = session.Ixnetwork
+        tg = TrafficGenerator(ixNetwork=ixNetwork)
+        tg.clear_counters()
+
+    def get_quick_flow_groups():
+        pass
+        
+
